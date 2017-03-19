37f71233a72523da797b92c884e59ef2b724e203
Aerelloth <oceane.scheidel@gmail.com>
2017-03-19 19:07:33 +0100

    Ajout des fichiers

---
+++ resources/js/scripts.js
@@ -0,0 +1,48 @@
+$(function () {
+
+	/* Scroll progressif */
+
+  $('.scroll').on('click', function(e) {
+
+    e.preventDefault();
+
+    var hash = this.hash;
+
+    $('html, body').animate({
+
+      scrollTop: $(this.hash).offset().top
+
+    }, 1000, function(){
+
+      window.location.hash = hash;
+
+    });
+
+  });
+
+  /* Options Masonry */
+
+  var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true,
+    transitionDuration: '1s'
+  });
+
+  /* Agrandissement du bloc au clic */
+  $('.description').on('click', function() {
+
+	var $gridItem = $(this).parent().parent();
+
+    if ($gridItem.hasClass('double-grid')) {
+      		$gridItem.removeClass('double-grid');
+    }
+    else {
+      		$gridItem.addClass('double-grid');
+    }
+
+    $grid.masonry();
+
+    });
+
+});
